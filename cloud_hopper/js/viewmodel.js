5c6ee9283ffc8ce2a4204becd69ae95be5195d2a
iDimitrova1 <100266974+iDimitrova1@users.noreply.github.com>
2026-06-04 21:30:25 +0300

    Update viewmodel.js

--- cloud_hopper/js/viewmodel.js
+++ cloud_hopper/js/viewmodel.js
@@ -27,7 +27,7 @@ textureLoader.load(
 
         // 5. Calibration Positioning
         // X: push to the right, Y: pull down, Z: place slightly in front of lens
-        knifeSprite.position.set(-0.1, 1, -0.8); 
+        knifeSprite.position.set(-0.12, -0.2, -0.8); 
 
         // Add the flat mesh to our animated viewmodel group
         viewmodel.add(knifeSprite);
